de7289f7e5f93d20152574e5ed51e5358358f26d
thenghia <nghiadang085@gmail.com>
2023-08-18 10:19:24 +0700

    Add file config-overrides

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The very important thing you should have is the patience
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Thenghia_rr
         </a>
       </header>
     </div>
